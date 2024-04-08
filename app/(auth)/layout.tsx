5588b71348f69e61e594f1ed2ccaefdf0f26ba93
surajjayraman <suraj.jayraman@gmail.com>
2024-04-08 16:12:07 -0400

    Update auth folder

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,8 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Auth",
-  description: "Admin dasboard to manage Borcelle's data",
+  title: "Borcelle - Store Auth",
+  description: "Borcelle Ecommerce Store",
 };
 
 export default function RootLayout({
