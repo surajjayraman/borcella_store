298e825a5dca21f9e52c503abdbdf5accd6a973d
surajjayraman <suraj.jayraman@gmail.com>
2024-04-08 18:51:33 -0400

    Update Navbar for conditional rendering of Menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
-import { ShoppingCart } from "lucide-react";
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { Menu, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const { user } = useUser();
+
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex justify-between items-center bg-white">
       <Link href="/">
@@ -21,6 +26,8 @@ const Navbar = () => {
           <ShoppingCart />
           <p className="text-base-bold">Cart (0)</p>
         </Link>
+
+        {user && <Menu className="cursor-pointer" />}
       </div>
     </div>
   );
