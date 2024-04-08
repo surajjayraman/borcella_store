8fd5717edf7171a29f3c5ad2aee651de5598cd09
surajjayraman <suraj.jayraman@gmail.com>
2024-04-08 18:10:34 -0400

    Update component Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex justify-between items-center bg-white">
-      Navbar
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={130} height={100} />
+      </Link>
+
+      <div>
+        <Link href="/">Home</Link>
+      </div>
+
+      <div>
+        <Link href="/cart">
+          <ShoppingCart />
+        </Link>
+      </div>
     </div>
   );
 };
