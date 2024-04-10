fa5acc88c1bbd208c55030fc2287b58bee41bc9b
surajjayraman <suraj.jayraman@gmail.com>
2024-04-10 13:31:33 -0400

    Update dropdownMenu styles in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 import { useState } from "react";
 
 const Navbar = () => {
-    const { user } = useUser();
-    const [dropdownMenu, setDropdownMenu] = useState(false);
+  const { user } = useUser();
+  const [dropdownMenu, setDropdownMenu] = useState(false);
 
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex justify-between items-center bg-white">
@@ -20,7 +20,7 @@ const Navbar = () => {
         <Link href="/">Home</Link>
       </div>
 
-      <div>
+      <div className="relative flex gap-3 items-center">
         <Link
           href="/cart"
           className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
@@ -29,7 +29,22 @@ const Navbar = () => {
           <p className="text-base-bold">Cart (0)</p>
         </Link>
 
-        {user && <Menu className="cursor-pointer" />}
+        {user && (
+          <Menu
+            className="cursor-pointer"
+            onClick={() => setDropdownMenu(!dropdownMenu)}
+          />
+        )}
+        {user && dropdownMenu && (
+          <div className="absolute top-10 right-5 flex flex-col gap-2 p-3 rounded-lg border bg-white text-base-bold ">
+            <Link href="/wishlist" className="hover:text-red-1">
+              Wishlist
+            </Link>
+            <Link href="/orders" className="hover:text-red-1">
+              Orders
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
