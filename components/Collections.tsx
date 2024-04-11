b1377901f9ed1a66960dc36bf6a619920071f4e0
surajjayraman <suraj.jayraman@gmail.com>
2024-04-11 09:18:32 -0400

    Update Collections store component styles

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 
 const Collections = async () => {
   const collections = await getCollections();
-  //   console.log(`getCollections: ${JSON.stringify(collections)}`);
   return (
-    <div>
+    <div className="flex flex-col items-center gap-10 py-8 px-5">
       <p className="text-heading1-bold">Collections</p>
-      <div>
+      <div className="flex items-center justify-center gap-8">
         {collections?.map((collection: CollectionType) => (
           <Link href={`/collections/${collection._id}`} key={collection._id}>
             <Image
