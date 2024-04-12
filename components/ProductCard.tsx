5bf1fab21cca716da2f39552b58cfb6f2e15d2c2
surajjayraman <suraj.jayraman@gmail.com>
2024-04-12 12:15:35 -0400

    Update component ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
-  return <div>ProductCard</div>;
+  return (
+    <div>
+      <Link href={`/products/${product._id}`}>
+        <Image
+          src={product.media[0]}
+          alt={product.title}
+          width={250}
+          height={300}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default ProductCard;
