c6512e6db1a34ee08643921ce9039b1040b97a73
surajjayraman <suraj.jayraman@gmail.com>
2024-04-12 12:36:14 -0400

    Update component ProductCard styles

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -10,7 +10,15 @@ const ProductCard = ({ product }: { product: ProductType }) => {
           alt={product.title}
           width={250}
           height={300}
+          className="h-[250px] rounded-lg object-cover cursor-pointer"
         />
+        <div>
+          <p className="text-base-bold">{product.title}</p>
+          <p className="text-small-medium text-grey-2">{product.category}</p>
+              </div>
+              <div>
+                  
+              </div>
       </Link>
     </div>
   );
