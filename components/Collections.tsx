ac2d0826aeab0eef264442717c43efcdcb8c5f1f
surajjayraman <suraj.jayraman@gmail.com>
2024-04-12 10:48:40 -0400

    Update Collections with conditional rendering

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -7,19 +7,23 @@ const Collections = async () => {
   return (
     <div className="flex flex-col items-center gap-10 py-8 px-5">
       <p className="text-heading1-bold">Collections</p>
-      <div className="flex items-center justify-center gap-8">
-        {collections?.map((collection: CollectionType) => (
-          <Link href={`/collections/${collection._id}`} key={collection._id}>
-            <Image
-              src={collection.image}
-              alt={collection.title}
-              width={350}
-              height={200}
-              className="rounded-lg cursor-pointer"
-            />
-          </Link>
-        ))}
-      </div>
+      {!collections || collections.length === 0 ? (
+        <p className="text-body-bold">No collections found</p>
+      ) : (
+        <div className="flex items-center justify-center gap-8">
+          {collections?.map((collection: CollectionType) => (
+            <Link href={`/collections/${collection._id}`} key={collection._id}>
+              <Image
+                src={collection.image}
+                alt={collection.title}
+                width={350}
+                height={200}
+                className="rounded-lg cursor-pointer"
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
