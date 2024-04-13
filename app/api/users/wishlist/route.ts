798992dea4b661cce51b26d8a3e9f409c397dd8a
surajjayraman <suraj.jayraman@gmail.com>
2024-04-13 14:17:35 -0400

    Update api route for users wishlist

--- app/api/users/wishlist/route.ts
+++ app/api/users/wishlist/route.ts
@@ -1,10 +1,28 @@
+import User from "@/lib/models/User";
+import { auth } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    try {
+  try {
+    const userId = auth();
 
-    }catch (error) {
-        console.log("[wishlist_POST]", error);
-        return new NextResponse("Internal Server Error", { status: 500 });
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    const { productId } = await req.json();
+
+    if (!productId) {
+      return new NextResponse("Product Id required", { status: 400 });
+    }
+  } catch (error) {
+    console.log("[wishlist_POST]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 };
