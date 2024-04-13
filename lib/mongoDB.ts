5739660165eed1109ee030fbf5b70e3c46045f66
surajjayraman <suraj.jayraman@gmail.com>
2024-04-13 09:17:42 -0400

    Add mongoDB.ts

---
+++ lib/mongoDB.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDB = async (): Promise<void> => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL || "", {
+      dbName: "Borcelle_Admin",
+    });
+
+    isConnected = true;
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+  }
+};
+
+export default connectToDB;
