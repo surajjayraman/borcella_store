d6c43e05feaebf203ac6cfcb4af0bbc00bcf2d25
surajjayraman <suraj.jayraman@gmail.com>
2024-04-13 09:41:31 -0400

    Update api route for users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -13,9 +13,13 @@ export const GET = async (req: NextRequest) => {
     await connectToDB();
     let user = await User.findOne({ clerkId: userId });
 
+    // when the user signs in for the first time, create a new user
     if (!user) {
       user = await User.create({ clerkId: userId });
+      await user.save();
     }
+
+    return NextResponse.json(user, { status: 200 });
   } catch (error) {
     console.log("[users_GET]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
