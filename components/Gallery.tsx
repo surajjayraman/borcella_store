ad58a1e3a30097fa457bf8443f6a9c5884d8484e
surajjayraman <suraj.jayraman@gmail.com>
2024-04-14 13:08:38 -0400

    Update component Gallery styles

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 const Gallery = ({ productMedia }: { productMedia: string[] }) => {
-  return <div>Gallery</div>;
+  return (
+    <div className="flex flex-col gap-3 max-w-[500px]">
+      <Image
+        src={productMedia[0]}
+        width={500}
+        height={500}
+        alt="product"
+        className="w-96
+        h-96 rounded-lg shadow-xl object-cover"
+      />
+      <div className="flex gap-2 overflow-auto tailwind-scrollbar-hide">
+        {productMedia.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            width={200}
+            height={200}
+            alt="product"
+            className="w-20 h-20 rounded-lg object-cover cursor-pointer"
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
