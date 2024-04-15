18873c3f1c71caac5445c6f52159ba9380adbe36
surajjayraman <suraj.jayraman@gmail.com>
2024-04-15 16:48:52 -0400

    Update component Navbar with cart state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useCart from "@/lib/hooks/useCart";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { CircleUserRound, Menu, ShoppingCart } from "lucide-react";
 import Image from "next/image";
@@ -9,6 +10,8 @@ import { useState } from "react";
 const Navbar = () => {
   const { user } = useUser();
   const [dropdownMenu, setDropdownMenu] = useState(false);
+  const cart = useCart();
+
 
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex justify-between items-center bg-white">
@@ -26,7 +29,7 @@ const Navbar = () => {
           className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
         >
           <ShoppingCart />
-          <p className="text-base-bold">Cart (0)</p>
+          <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
         </Link>
 
         {user && (
