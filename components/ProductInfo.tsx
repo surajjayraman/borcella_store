1b33e861c9cd6c5b16fb0bdf53edb700e057e5e6
surajjayraman <suraj.jayraman@gmail.com>
2024-04-15 14:01:31 -0400

    Refactor component ProductInfo

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
-import { Heart } from "lucide-react";
-import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import HeartFavorite from "./HeartFavorite";
 
 const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
-  
   return (
     <div className="max-w-[400px] flex flex-col gap-4">
       <div className="flex justify-between items-center">
         <p className="text-heading3-bold">{productInfo.title}</p>
-        <button onClick={handleLike}>
-          <Heart fill={`${isLiked ? "red" : "white"}`} />
-        </button>
+        <HeartFavorite product={productInfo} />
       </div>
     </div>
   );
