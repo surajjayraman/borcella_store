84abcebe76f86182a47b2e857b4576645382c3a0
surajjayraman <suraj.jayraman@gmail.com>
2024-04-15 13:08:27 -0400

    Update component ProductInfo with title heart

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -1,7 +1,15 @@
+import { Heart } from "lucide-react";
 import React from "react";
 
 const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
-  return <div>ProductInfo</div>;
+  return (
+    <div className="max-w-[400px] flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+              <p className="text-heading3-bold">{productInfo.title}</p>
+              <Heart />
+      </div>
+    </div>
+  );
 };
 
 export default ProductInfo;
