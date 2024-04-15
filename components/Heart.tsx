08de666ced9520f35036ef8a321d9f3d7d07e281
surajjayraman <suraj.jayraman@gmail.com>
2024-04-15 13:55:38 -0400

    Rename component Heart to HeartFavorite

--- components/Heart.tsx
+++ components/Heart.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
 
-const Heart = () => {
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { Heart } from "lucide-react";
+
+const HeartFavorite = ({ product }: { product: ProductType }) => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch("/api/users");
+        const data = await res.json();
+        setSignedInUser(data);
+        setIsLiked(data.wishlist.includes(product._id));
+        setLoading(false);
+      } catch (err) {
+        console.log("[Heart_getUser]", err);
+      }
+    };
+    if (user) {
+      getUser();
+    }
+  }, [user, product._id]);
+
+  const handleLike = async () => {
+    try {
+      setLoading(true);
+      if (!signedInUser) {
+        router.push("/sign-in");
+        return;
+      } else {
+        const res = await fetch("/api/users/wishlist", {
+          method: "POST",
+          body: JSON.stringify({ productId: product._id }),
+        });
+        const updatedUser = await res.json();
+        setSignedInUser(updatedUser);
+        setIsLiked(updatedUser.wishlist.includes(product._id));
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log("[Heart_handleLike]", err);
+    }
+  };
   return (
-    <div>Heart</div>
-  )
-}
+    <div>
+      <button onClick={handleLike}>
+        <Heart fill={`${isLiked ? "red" : "white"}`} />
+      </button>
+    </div>
+  );
+};
 
-export default Heart
+export default Heart;
