569e71e28a364312718107edca27d2bfe0fd281e
surajjayraman <suraj.jayraman@gmail.com>
2024-04-17 13:43:18 -0400

    Update payment success page

--- app/(root)/payment_success/page.tsx
+++ app/(root)/payment_success/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const SuccessfulPayment = () => {
   return (
-    <div className=''>SuccessfulPayment</div>
-  )
-}
+    <div className="h-screen flex flex-col justify-center items-center gap-5">
+      <p className="text-heading4-bold text-red-1">Payment Successful</p>
+      <p>Thank You for your purchase</p>
+      <Link href="/" className="p-4 border text-base-bold">
+        CONTINUE TO SHOPPING
+      </Link>
+    </div>
+  );
+};
 
-export default SuccessfulPayment
+export default SuccessfulPayment;
