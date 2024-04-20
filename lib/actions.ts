b10653645b5ae7adc2f165e1ecf7a4e4ddd3491c
surajjayraman <suraj.jayraman@gmail.com>
2024-04-20 12:58:52 -0400

    Update seach query page

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,6 +5,13 @@ export const getCollections = async () => {
   return await collections.json();
 };
 
+export const getCollectionDetails = async (collectionId: string) => {
+  const collection = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`
+  );
+  return await collection.json();
+};
+
 export const getProducts = async () => {
   const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
   return await products.json();
@@ -15,4 +22,25 @@ export const getProductDetails = async (productId: string) => {
     `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`
   );
   return await product.json();
+};
+
+export const getSearchedProducts = async (query: string) => {
+  const searchedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/search/${query}`
+  );
+  return await searchedProducts.json();
+};
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`
+  );
+  return await orders.json();
+};
+
+export const getRelatedProducts = async (productId: string) => {
+  const relatedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`
+  );
+  return await relatedProducts.json();
 };
