2d5605072c9a1feea869d677c22c4d5e9c3d5a0f
surajjayraman <suraj.jayraman@gmail.com>
2024-04-20 14:11:56 -0400

    Update component Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full border-t-4 border-blue-500 border-solid h-12 w-12"></div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
