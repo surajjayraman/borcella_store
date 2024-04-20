eb7f53744cc445a5304a0f7b8a27f9cf79358b11
surajjayraman <suraj.jayraman@gmail.com>
2024-04-20 13:30:50 -0400

    Update component ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,30 +4,36 @@ import Image from "next/image";
 import Link from "next/link";
 import HeartFavorite from "./HeartFavorite";
 
-const ProductCard = ({ product }: { product: ProductType }) => {
+interface ProductCardProps {
+  product: ProductType;
+  updateSignedInUser?: (updatedUser: UserType) => void;
+}
+
+const ProductCard = ({ product, updateSignedInUser }: ProductCardProps) => {
   return (
-    <div>
-      <Link
-        href={`/products/${product._id}`}
-        className="w-[220px] flex flex-col gap-2"
-      >
-        <Image
-          src={product.media[0]}
-          alt={product.title}
-          width={250}
-          height={300}
-          className="h-[250px] rounded-lg object-cover cursor-pointer"
+    <Link
+      href={`/products/${product._id}`}
+      className="w-[220px] flex flex-col gap-2"
+    >
+      <Image
+        src={product.media[0]}
+        alt="product"
+        width={250}
+        height={300}
+        className="h-[250px] rounded-lg object-cover"
+      />
+      <div>
+        <p className="text-base-bold">{product.title}</p>
+        <p className="text-small-medium text-grey-2">{product.category}</p>
+      </div>
+      <div className="flex justify-between items-center">
+        <p className="text-body-bold">${product.price}</p>
+        <HeartFavorite
+          product={product}
+          updateSignedInUser={updateSignedInUser}
         />
-        <div>
-          <p className="text-base-bold">{product.title}</p>
-          <p className="text-small-medium text-grey-2">{product.category}</p>
-        </div>
-        <div className="flex justify-between items-center">
-          <p className="text-body-bold">${product.price}</p>
-          <HeartFavorite product={product} />
-        </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
